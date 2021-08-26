e68d7a8677b45dd9884930130d5ed830806a058f
GabrielAguiar277 <gabriel.aguiar.ti@outlook.com>
2021-08-26 18:16:36 -0300

    atualizacao imput terminal

--- terminal.js
+++ terminal.js
@@ -1,20 +1,99 @@
-const $sendButton = document.querySelector('.send-button')
-$sendButton.addEventListener('click', sendMessage)
+// const $sendButton = document.querySelector('.send-button')
+// $sendButton.addEventListener('click', sendMessage)
+
+// function sendMessage(e){
+//     e.preventDefault()
+//     const $input = document.querySelector('.user-command')
+//     const inputValue = $input.value
+
+//     makeMessage(inputValue)
+//     commands(inputValue)
+//     $input.value = ''
+//     $input.focus()
+// }
+
+// function makeMessage(message){
+//     const $fatherMessage = document.querySelector('.computer-terminal')
+//     const command = `Imac-de-Gabriel:~ gabrielaguiar$${message}`
+    
+//     $fatherMessage.innerHTML += `<div class="last-command"> ${command}</div>`
+// }
+
+// function commands(command){
+//     const $fatherMessage = document.querySelector('.computer-terminal')
+
+//     switch(command){
+//         case 'ls':
+//             $fatherMessage.innerHTML += `
+//             <div class="last-command">
+//                 <ul>
+//                     <li>Desktop</li>
+//                     <li>Documents</li>
+//                     <li>Downloads</li>
+//                     <li>Favorites</li>
+//                 </ul>
+//             </div>
+//             `
+//             break
+
+//         case 'cd Desktop':
+//             $fatherMessage.innerHTML += `
+//             <div class="last-command">
+//                 <ul>
+//                     <li>League of Legends</li>
+//                     <li>Visual Studio Code</li>
+//                     <li>Pycharm</li>
+//                     <li>IntelliJ</li>
+//                 </ul>
+//             </div>
+//             `
+//             break
+        
+//         case 'cd Documents':
+//             $fatherMessage.innerHTML += `
+//             <div class="last-command">
+//                 <ul>
+//                     <li>Lições</li>
+//                     <li>Estudo</li>
+//                     <li>GitHub</li>
+//                     <li>readme.txt</li>
+//                 </ul>
+//             </div>
+//             `
+//             break
+
+//         case 'clear':
+//             $fatherMessage.innerHTML = ''
+//             break
+        
+//         default:
+//             $fatherMessage.innerHTML += `<div class="last-command">'${command}' Comando não conhecido</div>`
+//             break
+//     }
+// }
+
+
+const $input = document.querySelector('.user-command')
+$input.addEventListener('keydown', sendMessage)
 
 function sendMessage(e){
-    e.preventDefault()
-    const $input = document.querySelector('.user-command')
-    const inputValue = $input.value
+    var tecla = e.keyCode
 
-    makeMessage(inputValue)
-    commands(inputValue)
-    $input.value = ''
-    $input.focus()
+    if(tecla == 13){
+        const $input = document.querySelector('.user-command')
+        const inputValue = $input.value
+
+        makeMessage(inputValue)
+        commands(inputValue)
+        $input.value = ''
+        $input.focus()
+    }
+    
 }
 
 function makeMessage(message){
     const $fatherMessage = document.querySelector('.computer-terminal')
-    const command = `Imac-de-Gabriel:~ gabrielaguiar$${message}`
+    const command = `Imac-Gabriel:~ gabrielaguiar$${message}`
     
     $fatherMessage.innerHTML += `<div class="last-command"> ${command}</div>`
 }
@@ -56,6 +135,7 @@ function commands(command){
                     <li>Lições</li>
                     <li>Estudo</li>
                     <li>GitHub</li>
+                    <li>projetos</li>
                     <li>readme.txt</li>
                 </ul>
             </div>
